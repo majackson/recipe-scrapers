fde72259d432e7f647b53674f1475825afd71152
Matt Jackson <me@mattjackson.eu>
2012-01-03 21:04:57 +0000

    settings: removed redundant settings items from aa package

--- recipe_scrapers/settings/__init__.py
+++ recipe_scrapers/settings/__init__.py
@@ -1,8 +1,8 @@
 import os
 
-if os.environ.get('AACONTEXT') == 'production':
+if os.environ.get('SCRAPERSCONTEXT') == 'production':
     from production import *
-elif os.environ.get('AACONTEXT') == 'staging':
+elif os.environ.get('SCRAPERSCONTEXT') == 'staging':
     from staging import *
 else: #assume development
     from development import *
